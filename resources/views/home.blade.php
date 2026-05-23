c3a5e7fb5368d082f835c1305d341d746ba3e972
harisahmed18403 <harisahmed18403@gmail.com>
2026-05-23 16:05:50 +0100

    remove extra components from home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,29 +2,13 @@
 
 @section('title', 'Grawlix Design')
 
-
 @section('content')
     <div class="mx-2 mb-6" style="height:520px;">
-        @include('home.i-can-do-anything')
-    </div>
+        <section class="flex w-full min-h-full relative">
+    <canvas id="icda-canvas" class="absolute inset-0 w-full h-full block"></canvas>
+</section>
 
-    <x-section-header title="Build a website for your business" />
-    <div class="border border-brand-red border-t-0 overflow-y-auto mx-2 mb-6 rounded-b" style="height:520px;">
-        @include('home.web-design')
-    </div>
+@vite('resources/js/i-can-do-anything.js')
 
-    <x-section-header title="Run your operations from one place" />
-    <div class="border border-brand-red border-t-0 overflow-y-auto mx-2 mb-6 rounded-b" style="height:520px;">
-        @include('home.enterprise-tools')
-    </div>
-
-    <x-section-header title="Sell on Amazon, eBay, and your own site" />
-    <div class="border border-brand-red border-t-0 overflow-y-auto mx-2 mb-6 rounded-b" style="height:520px;">
-        @include('home.e-commerce')
-    </div>
-
-    <x-section-header title="Get found on Google and fill your calendar" />
-    <div class="border border-brand-red border-t-0 overflow-y-auto mx-2 mb-6 rounded-b" style="height:520px;">
-        @include('home.online-business-integration')
     </div>
 @endsection
--- resources/views/home/i-can-do-anything.blade.php
+++
@@ -1,5 +0,0 @@
-<section class="flex w-full min-h-full relative">
-    <canvas id="icda-canvas" class="absolute inset-0 w-full h-full block"></canvas>
-</section>
-
-@vite('resources/js/i-can-do-anything.js')
