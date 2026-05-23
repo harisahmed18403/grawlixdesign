98540839f83825376b2fce86bcd3f3a934a2b23f
harisahmed18403 <harisahmed18403@gmail.com>
2026-05-23 16:19:16 +0100

    delete unused js and rename to home js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,10 @@
 @section('title', 'Grawlix Design')
 
 @section('content')
-    <div class="mx-2 mb-6" style="height:520px;">
-        <section class="flex w-full min-h-full relative">
-    <canvas id="icda-canvas" class="absolute inset-0 w-full h-full block"></canvas>
-</section>
-
-@vite('resources/js/i-can-do-anything.js')
-
-    </div>
+<div class="mx-2 mb-6" style="height:520px;">
+    <section class="flex w-full min-h-full relative">
+        <canvas id="icda-canvas" class="absolute inset-0 w-full h-full block"></canvas>
+    </section>
+    @vite('resources/js/home.js')
+</div>
 @endsection
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/ecommerce.js', 'resources/js/enterprise.js', 'resources/js/i-can-do-anything.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/home.js'],
             refresh: true,
         }),
         tailwindcss(),
